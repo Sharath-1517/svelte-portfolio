788f0b2813b8df6ae0320026d20cd14224cd6a2d
sharathvgts <sharath@vishgyana.com>
2023-10-06 11:00:33 +0530

    changed tech to svelte-skeleton

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { LightSwitch } from '@skeletonlabs/skeleton';
+  import logo from '../public/images/logo.svg';
+</script>
+
+<div class="bg-background">
+  <div class="container mx-auto flex justify-between items-center p-5">
+    <img src={logo} alt="Sharath lingam | Developer" class="object-contain shrink h-8 dark:invert-[100%]">
+    <div class="flex justify-end items-center gap-5 w-full">
+      <LightSwitch />
+    </div>
+  </div>
+</div>
+
+
+<style>
+  
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../app.postcss';
+	import Navbar from '../components/Navbar.svelte';
+</script>
+
+
+
+<header>
+	<Navbar />
+</header>
+<main>
+	<slot />
+</main>
